e0f53da74d5335ed193c1141466702d024927e2c
august von trapp <augustvontrapp@gmail.com>
2018-12-16 13:21:01 -0800

    add division logic to menger sponge

--- sketches/cc2_menger_sponge.js
+++ sketches/cc2_menger_sponge.js
@@ -2,21 +2,40 @@ let width = 800;
 let height = 800;
 
 class Box {
-  constructor(p, x,y,z,r_) {
+  constructor(p,x,y,z,r_) {
     this.pos = new p.createVector(x,y,z);
     this.r = r_;
+  }
+
+  generate(p) {
+    const boxes = [];
+    for (let x = -1; x < 2; x += 1) {
+      for (let y = -1; y < 2; y += 1) {
+        for (let z = -1; z < 2; z += 1) {
+          const newR = this.r/3;
+          const b = new Box(
+            p,
+            this.pos.x+x*newR,
+            this.pos.y+y*newR,
+            this.pos.z+z*newR,
+            newR
+          );
+          boxes.push(b)
+        }
+      }
+    }
+    return boxes;
   }
 
 
   show(p) {
     const pos = this.pos;
-    p.pushMatrix();
+    p.push();
     p.translate(pos.x, pos.y, pos.z);
-    p.box(r);
-    p.popMatrix();
+    p.box(this.r);
+
+    p.pop();
   }
-
-
 }
 
 
@@ -24,11 +43,24 @@ class Box {
 export default function sketch (p) {
   let rotation = 0;
   let b;
+  let sponge = [];
+  let next = [];
 
-  p.setup = function () {
+
+  p.setup = () => {
     p.createCanvas(width, height, p.WEBGL);
     b = new Box(p, 0,0,0,200);
+    sponge.push(b)
   };
+
+  p.mousePressed = () => {
+    sponge.forEach((box) => {
+      const newBoxes = box.generate(p);
+      next = [...next, ...newBoxes]
+    });
+
+    sponge = next;
+  }
 
 
   p.draw = function () {
@@ -37,7 +69,11 @@ export default function sketch (p) {
     p.noFill();
 
     p.rotateX(rotation);
-    p.box(100);
+    p.rotateY(rotation);
+
+    sponge.forEach((box) => {
+      box.show(p);
+    })
 
     rotation += 0.01;
   };
